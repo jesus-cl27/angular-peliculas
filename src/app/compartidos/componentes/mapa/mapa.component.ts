cb1d8baae516fc44ad132eb02cb21e102d38f159
jesus-cl27 <jcruz2711@outlook.es>
2025-04-15 00:31:22 -0600

    Feat: Add functionality to display cinemas with their respective name in the map component

--- src/app/compartidos/componentes/mapa/mapa.component.ts
+++ src/app/compartidos/componentes/mapa/mapa.component.ts
@@ -13,9 +13,14 @@ export class MapaComponent implements OnInit{
   ngOnInit(): void {
     this.capas = this.coordenadasIniciales.map( valor => {
       const marcador = marker([valor.latitud, valor.longitud], this.markerOptions);
+      if (valor.texto){
+        marcador.bindPopup(valor.texto, {autoClose: false, autoPan: false});
+      }
       return marcador;
     });
   }
+  @Input()
+  soloLectura = false;
 
   @Input()
   coordenadasIniciales: Coordenada[] = [];
@@ -43,6 +48,9 @@ export class MapaComponent implements OnInit{
   
   capas: Marker<any>[] = [];
   manejarClick(event: LeafletMouseEvent){
+    if (this.soloLectura){
+      return;
+    }
     const latitud = event.latlng.lat;
     const longitud = event.latlng.lng;
 
